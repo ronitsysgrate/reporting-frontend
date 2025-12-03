5fba5256c8167bc618f86500fb932dc0bbba1711
ronitsysgrate <ronit.philip@sysgrate.com>
2025-12-03 12:46:04 +0530

    server url updated

--- services/serverURL.ts
+++ services/serverURL.ts
@@ -1,5 +1,5 @@
 // const server_url: string = 'http://192.168.3.111:8080'
-const server_url: string = 'https://nm3pnvvs-8080.inc1.devtunnels.ms'
+const server_url: string = 'https://reporting-backend-eight.vercel.app'
 // const server_url: string = 'http://localhost:8080'
 
 export default server_url
