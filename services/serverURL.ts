513786ae013167d7d46066d2860cbf31cc0a10cf
ronitsysgrate <ronit.philip@sysgrate.com>
2026-01-07 10:51:48 +0530

    patch

--- services/serverURL.ts
+++ services/serverURL.ts
@@ -1,5 +1,5 @@
-// const server_url: string = 'http://192.168.3.111:8080'
-const server_url: string = 'https://reporting-backend-0eh4.onrender.com'
-// const server_url: string = 'http://localhost:8080'
+// const server_url: string = 'http://172.28.6.34:8080'
+// const server_url: string = 'https://reporting-backend-0eh4.onrender.com'
+const server_url: string = 'http://localhost:8080'
 
 export default server_url
